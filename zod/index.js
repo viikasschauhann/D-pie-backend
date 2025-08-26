e4cd277f6b10c0f2545e94f92b89f52b775fed19
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2025-08-26 15:11:26 +0530

    Add zod schema

---
+++ zod/index.js
@@ -0,0 +1,61 @@
+import { z } from "zod";
+
+const signupSchema = z.object({
+    name: z.string().min(1).max(50).nullish(),
+    email: z.string().min(5).max(50).email(),
+    password: z.string().min(5).max(50),
+    role: z.enum(["STUDENT", "TUTOR"]),
+    otp: z.string().min(6).max(6).nullish()
+}).superRefine(({password}, checkPassComplexity) => {
+    const containsUpperCase = (ch) => /[A-Z]/.test(ch);
+    const containsLowerCase = (ch) => /[a-z]/.test(ch);
+    const containsSpecialChar = (ch) => /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/.test(ch);
+
+    let countOfUpperCase = 0, countOfLowerCase = 0, countOfSpecialChar = 0, countOfNumber = 0;
+
+    for(let i = 0; i < password.length; i++) {
+        let ch = password.charAt(i);
+        if(!isNaN(+ch)) countOfNumber++;
+        if(containsLowerCase(ch)) countOfLowerCase++;
+        if(containsUpperCase(ch)) countOfUpperCase++;
+        if(containsSpecialChar(ch)) countOfSpecialChar++;
+    }
+
+    let errObj = {
+        upperCase: {pass: true, message:"Add upper case."},
+        lowerCase: {pass: true, message:"Add lower case."},
+        specialChar: {pass: true, message: "Add special character."},
+        totalNumber: {pass: true, message: "Add number."}
+    };
+
+    if(countOfLowerCase < 1) {
+        errObj = {...errObj, lowerCase: {...errObj.lowerCase, pass: false}};
+    }
+    if(countOfUpperCase < 1) {
+        errObj = {...errObj, upperCase: {...errObj.upperCase, pass: false}};
+    }
+    if(countOfSpecialChar < 1) {
+        errObj = {...errObj, specialChar: {...errObj.specialChar, pass: false}};
+    }
+    if(countOfNumber < 1) {
+        errObj = {...errObj, totalNumber: {...errObj.totalNumber, pass: false}};
+    }
+
+    if(countOfLowerCase < 1 || countOfUpperCase < 1 || countOfSpecialChar < 1 || countOfNumber < 1) {
+        checkPassComplexity.addIssue({
+            code: "custom",
+            path: ["password"],
+            message: errObj.toString()
+        });
+    }
+});
+
+const signinSchema = z.object({
+    email: z.string().min(5).max(50).email(),
+    password: z.string().min(5).max(50)
+});
+
+export {
+    signupSchema,
+    signinSchema
+}
